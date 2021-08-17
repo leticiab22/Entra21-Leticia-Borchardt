e2530183acbe65e312970bc5b56f7df2a1765469
LETICIA BORCHARDT <leticiaborchardt123@gmail.com>
2021-08-17 16:58:13 -0300

    atualizado

--- Aula 62/teste/database/index.js
+++ Aula 62/teste/database/index.js
@@ -2,7 +2,22 @@ require("dotenv").config()
 const { Sequelize } = require("sequelize")
 const dbConfig = require("../config/database")
 
+const Usuario = require("../models/Usuario")
+const Projeto = require("../models/Projeto")
+const Endereco = require("../models/Endereco")
+
 const sequelize = new Sequelize(dbConfig);
+
+
+// Inicializando os models
+Usuario.init(sequelize)
+Projeto.init(sequelize)
+Endereco.init(sequelize)
+
+// Definindo as associações para os models
+Usuario.associate(sequelize.models)
+Projeto.associate(sequelize.models)
+Endereco.associate(sequelize.models)
 
 module.exports = sequelize;
 
@@ -17,8 +32,6 @@ module.exports = sequelize;
 //         sequelize.close()
 //     }
 // })();
-
-
 
 
 // terminal -> node .\database\index.js 
