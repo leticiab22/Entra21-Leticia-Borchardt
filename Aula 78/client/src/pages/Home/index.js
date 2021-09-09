02ad7710102cf8ee0f1b5c3b8122c103226f7384
LETICIA BORCHARDT <leticiaborchardt123@gmail.com>
2021-09-09 17:21:05 -0300

    atualizado

--- Aula 78/client/src/pages/Home/index.js
+++ Aula 78/client/src/pages/Home/index.js
@@ -1,18 +1,24 @@
-import { Redirect } from "react-router-dom";
+import { useState } from "react";
+import { Redirect, useHistory } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
-
+import "./styles.css";
 
 export function Home(props) {
     const { accessToken } = useAuth();
+    const history = useHistory();
         
     if (accessToken) {
         return <Redirect to="/dashboard" />        
     }
 
+
+
     return (
       <>        
-        <h1>Home</h1>
-        <p>{props.text}</p>       
+        <h1 className="home-title">Home</h1>
+        <hr/>
+        <p>{props.text}</p>     
+        <button className="btn btn1" onClick={() => history.push("/login")}>Logar</button>  
       </>
     );  
 }
